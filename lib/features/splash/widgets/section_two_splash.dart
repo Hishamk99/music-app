caf91eeb06cc5dddf216941105fe681ff875f7bc
hishamkame <hishamkamel370@gmail.com>
2024-06-10 17:02:18 +0300

    finish splash page

--- lib/features/splash/widgets/section_two_splash.dart
+++ lib/features/splash/widgets/section_two_splash.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-
 import 'custom_splash_button.dart';
+
 
 class SectionTwoSplash extends StatelessWidget {
   const SectionTwoSplash({super.key});
 
   @override
   Widget build(BuildContext context) {
-    
     return Stack(
       clipBehavior: Clip.none,
       children: [
