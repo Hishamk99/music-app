365d3d4ba5aa106e5d12ad6bdcf40ee624662849
hishamkame <hishamkamel370@gmail.com>
2024-06-16 22:17:23 +0300

    add audioplayers package

--- lib/features/home/data/singer_list.dart
+++ lib/features/home/data/singer_list.dart
@@ -2,30 +2,35 @@ import 'package:music_app/features/home/models/singer_model.dart';
 
 class SingerList {
   static List<SingerModel> singersList = [
-      SingerModel(
-        image: 'assets/images/selena.png',
-        name: 'Selena Gomez',
-        songName: 'Who Says',
-      ),
-      SingerModel(
-        image: 'assets/images/bebber.png',
-        name: 'Justin Bieber',
-        songName: 'SORRY',
-      ),
-      SingerModel(
-        image: 'assets/images/alan.jfif',
-        name: 'Alan Walker',
-        songName: 'Sing Me To Sleep',
-      ),
-      SingerModel(
-        image: 'assets/images/pitbull.jfif',
-        name: 'PitBull',
-        songName: 'Give Me Everything',
-      ),
-      SingerModel(
-        image: 'assets/images/the-weekend.jfif',
-        name: 'The Weeknd',
-        songName: 'Blinding Lights',
-      ),
-    ];
+    SingerModel(
+      image: 'assets/images/selena.png',
+      name: 'Selena Gomez',
+      songName: 'Who Says',
+      path: '',
+    ),
+    SingerModel(
+      image: 'assets/images/bebber.png',
+      name: 'Justin Bieber',
+      songName: 'SORRY',
+      path: '',
+    ),
+    SingerModel(
+      image: 'assets/images/alan.jfif',
+      name: 'Alan Walker',
+      songName: 'Sing Me To Sleep',
+      path: '',
+    ),
+    SingerModel(
+      image: 'assets/images/pitbull.jfif',
+      name: 'PitBull',
+      songName: 'Give Me Everything',
+      path: '',
+    ),
+    SingerModel(
+      image: 'assets/images/the-weekend.jfif',
+      name: 'The Weeknd',
+      songName: 'Blinding Lights',
+      path: '',
+    ),
+  ];
 }
--- lib/features/home/models/singer_model.dart
+++ lib/features/home/models/singer_model.dart
@@ -1,11 +1,20 @@
+import 'package:audioplayers/audioplayers.dart';
+
 class SingerModel {
   final String image;
   final String name;
   final String songName;
+  final String path;
 
-  SingerModel({
+  SingerModel( {
     required this.image,
     required this.name,
     required this.songName,
+    required this.path,
   });
+  void playSound()
+  {
+    final player = AudioPlayer();
+    player.play(AssetSource(path));
+  }
 }
